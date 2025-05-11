dbaf10af615a78e7dca1419349c535a0402663ce
roothider <158083651+roothider@users.noreply.github.com>
2025-05-12 00:13:26 +0800

    version 2.4.5.18
    1: fix spinlock panic for ios15 arm64e devices
    
    2: support tweak injection for WebContent process on ios16
    
    more info: https://github.com/roothide/Dopamine2-roothide/releases/tag/2.4.5.18

--- Application/Dopamine/Extensions/NSString+Version.m
+++ Application/Dopamine/Extensions/NSString+Version.m
@@ -16,14 +16,15 @@
     NSArray *components = [self componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]];
     
     components = [components filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF != ''"]];
-    assert(components.count <= 3);
+    assert(components.count <= 4);
     
-    while (components.count < 3)
+    while (components.count < 4)
         components = [components arrayByAddingObject:@"0"];
 
-    numericalRepresentation |= [components[0] integerValue] << 16;
-    numericalRepresentation |= [components[1] integerValue] << 8;
-    numericalRepresentation |= [components[2] integerValue];
+    numericalRepresentation |= [components[0] integerValue] << 24;
+    numericalRepresentation |= [components[1] integerValue] << 16;
+    numericalRepresentation |= [components[2] integerValue] << 8;
+    numericalRepresentation |= [components[3] integerValue];
     return numericalRepresentation;
 }
 
